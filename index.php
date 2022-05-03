aaf3dbdc92cc864d940a895b4e9efafeb8f21933
Pedro Augusto <pedro.august150@gmail.com>
2022-05-03 16:48:28 -0300

    comfirma

--- index.php
+++ index.php
@@ -4,6 +4,20 @@
 <meta charset="utf-8"/>
 </head>
 <body>
-      
+<form class="formulario" method="post" action="pages/dados.php">
+        <legend class="titulo">Preencha Com Os Dados Do Aluno</legend>
+        <input name="nome" class="campo" placeholder="Nome">
+<br><br>
+<input name="matricula" class="campo" placeholder="Matrícula">
+<br><br>
+<input name="cpf" class="campo" placeholder="CPF">
+<br><br>
+<input name="rg" class="campo" placeholder="Rg">
+<br><br>
+<input class="enviar" type="submit" value="Enviar">
+
+
+
+    </form> 
 </body>
 </html>
