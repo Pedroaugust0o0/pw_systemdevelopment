ea8b53553f4c0b77841b919030adb3515013e4c4
Mariaalice04 <mariaalic345@gmail.com>
2022-05-04 11:56:50 -0300

    commit

--- index.php
+++ index.php
@@ -1,85 +1,19 @@
 <html>
 <head>
-<title>PHP Test</title>
-<link rel="stylesheet" href="style.css">
+<title>Sorveteria</title>
+<link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="index.html">
 </head>
 <body>
+<header>
+  <h1>Nossos Sabores</h1>
+</header>
 
-<?php
+<nav>
 
-
-include_once "aluno.class.php";
-
-$novo = new aluno("123","Por","Gato","Marketing");
-
-$mat = "";
-
-$novo-> setMatricula($mat);
-
-  echo ($novo->getMatricula());
-
- 
-
-    $nom = "";
-
-$novo-> setNome($nom);
-
-  echo ($novo->getNome());
+  <a href="">Produtosfkk</a>
+</nav>
 
 
-$registro = 12121212121;
-
-$novo-> setRg($registro);
-
- ($novo->getRg());
-$dpt = "";
-
-$novo-> setDepartamento($dpt);
-
- echo ($novo->getDepartamento());
-
-
-
-   ?> 
-
-
-  </body>
-</html>
-
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <legend>Formulário</legend>
-    <link rel="stylesheet" href="style.css" type="text/css">
-</head>
-
-<body>
-
-
-
-    <form class="formulario" method="post" action="pages/dados.php">
-        
-        <br><br><br><br><br>
-        <title class="titulo">Preencha Com Os Dados Do Aluno</title>
-        <br><br>
-        <input name="nome" class="campo" placeholder="Nome">
-<br><br>
-<input name="matricula" class="campo" placeholder="Matrícula">
-<br><br>
-<input name="cpf" class="campo" placeholder="CPF">
-<br><br>
-<input name="rg" class="campo" placeholder="Rg">
-<br><br>
-<input class="enviar" type="submit" value="Enviar">
-
-
-
-    </form> 
-
-    
-    
 </body>
 </html>
