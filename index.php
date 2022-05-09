5e0c33b003cde6a50af01242e09c5b36b43cd33b
Pedro Augusto <pedro.august150@gmail.com>
2022-05-09 15:27:22 -0300

    commit

--- index.php
+++ index.php
@@ -3,53 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Venha e prove todos os nossos sabores"> 
+    <meta name="description" content="Venha e prove todos os nossos sabores">
     <title>Sorveteria</title>
+    <link rel="stylesheet" type="text/css" href="css/estyle.css">
 </head>
 <body>
     <header class="cabeçalho">
-     <h1> Venha conheçer nosso estabelecimento e nossos sabores!!</h1>
-    <img class="logo" src="logo.src" alt="Logo da sorveteria" >
+            <h1 class="titulo1"> Venha conheçer nosso estabelecimento e nossos sabores!!</h1>
+            <img class="logo" src="./img/logo.src.png" alt="Logo da sorveteria">
     </header>
+    <div class="container">
+        <nav class="menu-principal">
+            <h2>Confira nossos sabores abaixo</h2>
+            <ul>
+                <li href>Sabores</li>
+                <li href>Cadastre-se</li>
+                <li href>Login</li>
+            </ul>
+        </nav>
+    </div>
+    <section>
 
-<main class="menu-principal">
-   <h2 class="lista">Confira nossos sabores abaixo</h2> 
-   <p>Morango</p>
-   <p>chocolate</p>
-   <p>Creme</p>
-   <p>Napolitano</p>
-   <p>Baunilha</p>
-   <p>Pistache</p>
-   <p>Menta</p>
-   <p>Creme com passas</p>
-   <p>Maracunjá</p>
-   <p>Franboesa</p>
-   <p>Amora</p>
-   <p>Mirtilo</p>
-   <p>Cereja</p>
-   <p>Abacaxi</p>
-   <p>Açai</p>
-   <p>Caramelo</p>
-   <p>Pavê</p>
-   <p>Nata goiaba</p>
-   <p>Caja</p>
-   <p>Brigadeiro</p>
-   <p>Bombom</p>
-   <p>Flocos</p>
-   <p>Abacaxi com menta</p>
-   <p>Frutas vermelhas</p>
-   <p>Limão</p>
-   <p>Banana</p>
-   <p>Champagne</p>
-   <p>Chocomint</p>
-    
-</main>
-
+    </section>
     <footer>
         <p>Siganos nas redes sociais e fique por dentro de tudo</p>
         <a>Instagram</a>
         <a>Facebook</a>
-
     </footer>
 </body>
 </html>
