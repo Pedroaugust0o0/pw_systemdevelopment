8104e81f58f224e4b36a0a31388b1d0358f1148d
Mariaalice04 <mariaalic345@gmail.com>
2022-05-11 16:57:50 -0300

    commit

--- index.php
+++ index.php
@@ -1,38 +1,44 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
-
+<html lang="pt,BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Venha e prove todos os nossos sabores">
     <title>Sorveteria</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    <header class="cabecalho">
-        <h1 class="titulo1">Venha E conheça o nosso estabelecimento!</h1>
-        <div>
-        <img class="logo-milkshake" src="./img/download-removebg-preview.
-png" alt="Logo da sorveteria">
-        </div>
-    </header>
-    <div class="container">
-        <nav class="menu-principal">
-            <h2>Aproveite ao máximo, faça seu cadastro!</h2>
-            <ul class="tabela">
-                <a href="pages/cad_cliente.php">Cadastre-se</a>
-                <a href="pages/cadproduto.php">Sabores</a>
-                <a href="pages/login.php">login</a>
-            </ul>
-        </nav>
-    </div>
-    <section>
-
-    </section>
-    <footer class="rodape">
-        <p>Nos acompanhe nas nossas redes sociais e fique por dentro de tudo!</p><br></br>
-        <br><a>Instagram</a></br>
-        <br><a>Facebook</a></br>
-    </footer>
+    
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
