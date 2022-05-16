e2f76c81208edaee21765e56c4db779ccbc70637
Mariaalice04 <mariaalic345@gmail.com>
2022-05-16 11:35:33 -0300

    commit

--- index.php
+++ index.php
@@ -3,28 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Sorveteria</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
-
 <body>
 <header> 
+    <div>
  <nav>
-  <li>Cadastro</li>
-  <li>Login</li>
-  <li>Sabores</li>
+  <a type="text/page" href="cad_cliente.php">Cadastro</a>
+  <a type="pages" href="login.php">Login</a>
+  <a type="pages" href="sabores.php">Sabores</a>    
  </nav>
-
+    </div>
  <h1>Gelateria</h1>
 </header>
 
 <main>
-
+<h1>sorvetes<h1>
+<h1>Artesanais<h1>
+<button>Menu</button>
+<img src="sorvet.rsc-removebg-preview.png" alt="sorvete">
 </main>
-
-<footer>
-
-</foote>
 
 </body>
 </html>
