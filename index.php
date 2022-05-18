e5782ae15e436ec53e7de03516c1d17f307ada53
Mariaalice04 <mariaalic345@gmail.com>
2022-05-18 14:09:22 -0300

    commit

--- index.php
+++ index.php
@@ -4,25 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sorveteria</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 <header class="cabeçalho"> 
     <div>
- <nav class="menu_principal">
-  <a type="text/pages" href="cad_cliente.php">Cadastro</a>
-  <a type="text/pages" href="login.php">Login</a>
-  <a type="text/pages" href="sabores.php">Sabores</a>    
- </nav>
+<nav class="menu_principal">
+  <a type="text/pages" href="pages/cad_cliente.php">Cadastro</a>
+  <a type="text/pages" href="pages/login.php">Login</a>
+  <a type="text/pages" href="pages/sabores.php">Sabores</a>    
+</nav>
     </div>
- <h1>Gelateria</h1>
+ <h1 class ="título_empresa">Gelateria</h1>
 </header>
 <main class="conteudo_prinpial">
-<h1>sorvetes<h1>
-<h1>Artesanais<h1>
-<button class ="botão">Menu</button>
-<img src="sorvet.rsc-removebg-preview.png" alt="sorvete">
+ <h2>sorvetes<h2>
+ <h3>Artesanais<h3>
+ <button class ="botão">Menu</button>
 </main>
+  <img class="sorvete" src="img/sorvet.rsc-removebg-preview.png" alt="sorvete">
 </body>
 </html>
 
