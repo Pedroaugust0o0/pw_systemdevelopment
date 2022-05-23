067579d4d8bf86accdabe3726a2ea53ec7a96bab
Mariaalice04 <mariaalic345@gmail.com>
2022-05-23 11:32:52 -0300

    commit

--- pages/cad_cliente.php
+++ pages/cad_cliente.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Cadastro do cliente</title>
+    <link rel="stylesheet" type="text/css" href="css/cliente.css">
 </head>
 <body>
     <h1>login</h1>
@@ -12,46 +13,32 @@
     <legend><b>Fórmulário de cadastro</b></legend>
     <br>
     <div class="inputBox">
-        <input type="text" name="nome" id="nome" class="inputUser" required>
+        <input type="text" name="nome" id="nome" required>
         <label for="nome" class="labelInput">Nome completo</label>
+                    <br></br>
+        <input type="email" name="email" id="email" required>
+        <label for="nome" class="labelInput">Email</label>
+                    <br></br>
+        <input type="password" name="senha" id="senha" required>
+        <label for="nome" class="labelInput">Senha</label> 
+                    <br></br>
+        <input type="number" name="cpf" \
+			pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" \
+			title="Digite um CPF no formato: xxx.xxx.xxx-xx">
+        <label for="nome" class="labelInput">CPF</label>
+                    <br></br>
+        <input type="date" name="dtns" id="dtns" required>
+        <label for="nome" class="labelInput">Data de nascimento</label>
+                    <br></br>
+        <input type = "checkbox" id = "sexo" name = "sexo" value = "sexo">
+        <label for="nome" class="labelInput">Masculino</label>
+                    <br></br>
+        <input type = "checkbox" id = "sexo" name = "sexo" value = "sexo">
+        <label for="nome" class="labelInput">Feminino</label>
+                    <br></br>
+        <input type="submit" name="button" id="submit">
     </div>
-    <br><br>
-    <div class="inputBox">
-        <input type="text" name="email" id="email" class="inputUser" required>
-        <label for="email" class="labelInput">Email</label>
-    </div>
-    <br><br>
-    <div class="inputBox">
-        <input type="tel" name="telefone" id="telefone" class="inputUser" required>
-        <label for="telefone" class="labelInput">Telefone</label>
-    </div>
-    <p>Sexo:</p>
-    <input type="radio" id="feminino" name="genero" value="feminino" required>
-    <label for="feminino">Feminino</label>
-    <br>
-    <input type="radio" id="masculino" name="genero" value="masculino" required>
-    <label for="masculino">Masculino</label>
-    <br>
-    <input type="radio" id="outro" name="genero" value="outro" required>
-    <label for="outro">Outro</label>
-    <br><br>
-    <label for="data_nascimento"><b>Data de Nascimento:</b></label>
-    <input type="date" name="data_nascimento" id="data_nascimento" required>
-    <br><br><br>
-    <div class="inputBox">
-        <input type="text" name="cidade" id="cidade" class="inputUser" required>
-        <label for="cidade" class="labelInput">Cidade</label>
-    </div>
-    <br><br>
-    <div class="inputBox">
-        <input type="text" name="estado" id="estado" class="inputUser" required>
-        <label for="estado" class="labelInput">Estado</label>
-    </div>
-    <br><br>
-    <div class="inputBox">
-        <input type="text" name="endereco" id="endereco" class="inputUser" required>
-        <label for="endereco" class="labelInput">Endereço</label>
-    </div>
-    <br><br>
-    <input type="submit" name="submit" id="submit">
 </fieldset>
+</form>
+</body>
+</html>
