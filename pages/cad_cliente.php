628f8c33c1a42076e1c6fe56bf3f8c7c8f631453
Mariaalice04 <mariaalic345@gmail.com>
2022-05-24 15:47:05 -0300

    commit

--- pages/cad_cliente.php
+++ pages/cad_cliente.php
@@ -37,6 +37,9 @@
         <input type = "checkbox" id = "sexo" name = "sexo" value = "sexo">
         <label for="nome" class="labelInput">Feminino</label>
                     <br></br>
+        <input type = "checkbox" id = "sexo" name = "sexo" value = "sexo">
+        <label for="nome" class="labelInput">trans neutre</label>
+                    <br></br>
         <input type="submit" name="button" id="submit">
     </div>
 </fieldset>
