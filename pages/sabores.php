2404510c24d95ff73548fe2ef8a2b6d53b337c90
Mariaalice04 <mariaalic345@gmail.com>
2022-05-30 15:28:25 -0300

    commit

--- pages/sabores.php
+++ pages/sabores.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../css/cliente.css">
+    <link rel="stylesheet" type="text/css" href="../css/produto.css">
     <title>Sabores</title>
 </head>
 <body>
@@ -13,19 +13,14 @@
     <h1>Sabores de sorvete</h1>
     <img src="img/sorveteoreo-removebg-preview" alt="Sorvete de oreo">
     <h3>OREO</h3>
-
     <img src="img/chocolat-icecream-removebg-preview" alt="Sorvete de chocolate">
     <h3>CHOCOLATE E SAL MARINHO</h3>
-
     <img src="img/morangosort-removebg-preview" alt="Sorvete de morango">
     <h3>MORANGO BAUNILHA</h3>
-
     <img src="img/cheesecake-de-goiabada-removebg-preview" alt="Sorvete de chessecake">
     <h3>CHESSECAKE E GOIABADA</h3>
-
     <img src="img/pistache-removebg-preview" alt="Sorvete de pistache">
     <h3>PISTACHE</h3>
-
     <img src="img/blueberrysorvete-removebg-preview" alt="Sorvete de mirtilo">
     <h3>MIRTILO</h3>
 </body>
