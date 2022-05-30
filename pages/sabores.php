f4439c82db03996e1fd8d9810abac9aff233c0c9
Mariaalice04 <mariaalic345@gmail.com>
2022-05-30 15:40:19 -0300

    commit

--- pages/sabores.php
+++ pages/sabores.php
@@ -11,17 +11,17 @@
     <h2>Nossos</h2>
     </h1>Sabores</h1>
     <h1>Sabores de sorvete</h1>
-    <img src="img/sorveteoreo-removebg-preview" alt="Sorvete de oreo">
+    <img src="img/sorveteoreo-removebg-preview.png" alt="Sorvete de oreo">
     <h3>OREO</h3>
-    <img src="img/chocolat-icecream-removebg-preview" alt="Sorvete de chocolate">
+    <img src="img/chocolat-icecream-removebg-preview.png" alt="Sorvete de chocolate">
     <h3>CHOCOLATE E SAL MARINHO</h3>
-    <img src="img/morangosort-removebg-preview" alt="Sorvete de morango">
+    <img src="img/morangosort-removebg-preview.png" alt="Sorvete de morango">
     <h3>MORANGO BAUNILHA</h3>
-    <img src="img/cheesecake-de-goiabada-removebg-preview" alt="Sorvete de chessecake">
+    <img src="img/cheesecake-de-goiabada-removebg-preview.png" alt="Sorvete de chessecake">
     <h3>CHESSECAKE E GOIABADA</h3>
-    <img src="img/pistache-removebg-preview" alt="Sorvete de pistache">
+    <img src="img/pistache-removebg-preview.png" alt="Sorvete de pistache">
     <h3>PISTACHE</h3>
-    <img src="img/blueberrysorvete-removebg-preview" alt="Sorvete de mirtilo">
+    <img src="img/blueberrysorvete-removebg-preview.png" alt="Sorvete de mirtilo">
     <h3>MIRTILO</h3>
 </body>
 </html>
