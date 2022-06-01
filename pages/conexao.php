e9f3817e44f45c52ff2d0893fcb1acf4e5ebc77d
Pedro Augusto <pedro.august150@gmail.com>
2022-06-01 14:09:41 -0300

    commit

--- pages/conexao.php
+++ pages/conexao.php
@@ -8,6 +8,8 @@
  if(!$conexao){
      die("Houve um erro: ".mysqli_connect_error());
  }
+  
+ 
 
 //  echo 'tudo certo';
 ?>
--- pages/delet_cli.php
+++ pages/delet_cli.php
@@ -1,6 +1,17 @@
 <?php
- 
-    if(!empty($_GET['id']))
-    {
-          include_once('cad_cliente.php');
-    }
+    include 'conexao.php';
+
+    $sql = "select * from cliente";
+
+    $resultado = $conexao->query($sql);
+
+     foreach ($resultado as $key => $valor) {
+   // echo $valor['id'];
+     ?>
+       <tr><?php echo $valor['id']; ?></td>
+       <td><?php echo $valor['nome']; ?></td>
+       <td><?php echo $valor['email']; ?></td>
+       <td><?php echo $valor['cpf']; ?></td>
+       <td><?php echo $valor['dtns']; ?></td>
+       <td><?php echo $valor['sexo']; ?></td>
+     }
