c4a88bad95baaaa546b9042b88dc4bed2b160a98
Mariaalice04 <mariaalic345@gmail.com>
2022-06-01 16:55:55 -0300

    commit

--- pages/sabores.php
+++ pages/sabores.php
@@ -10,7 +10,7 @@
     <h1 class="menor">Nossos</h1>
     <h2 class="maior">Sabores</h2>
     <h1 class="medio">Sabores de sorvete</h1>
-    <img src="/img/amora.src.jpg" alt="Sorvet de amora">
+    <img src="../img/amora.src.jpg" alt="Sorvet de amora" class="amora">
     <h1 class="medio">Sabores de sorbete</h1>
 </body>
 </html>
