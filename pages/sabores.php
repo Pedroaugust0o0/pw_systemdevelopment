ca0a3c15eba556bbbf1f3dcc7b4d6710d2664adc
Mariaalice04 <mariaalic345@gmail.com>
2022-06-06 10:53:37 -0300

    COMMIT

--- pages/sabores.php
+++ pages/sabores.php
@@ -9,8 +9,24 @@
 <body>
     <h1 class="menor">Nossos</h1>
     <h2 class="maior">Sabores</h2>
-    <h1 class="medio">Sabores de sorvete</h1>
-    <img src="../img/amora.src.jpg" alt="Sorvet de amora" class="amora">
-    <h1 class="medio">Sabores de sorbete</h1>
+    <h1 class="medio">SABORES DE SORVETE</h1>
+    <img src="../img/sorveteoreo-removebg-preview.png" alt="oreo" class="oreo">
+    <h3>OREO</h3>
+    <P></P>
+    <img src="../img/chocolat-icecream-removebg-preview.png" alt="chocolate" class="chocolate">
+    <h3>CHOCOLATE</h3>
+    <p></p>
+    <img src="../img/morangosort-removebg-preview.png" alt="morango" class="morango">
+    <h3>MORANGO</h3>
+    <p></p>
+    <img src="../img/cheesecake-de-goiabada-removebg-preview.png" alt="cheesecake">
+    <h3>CHESEECAKE DE GOIABADA</h3>
+    <p></p>
+    <img src="../img/pistache-removebg-preview.png" alt="pistache">
+    <h3>PISTACHE</h3>
+    <img src="../img/blueberrysorvete-removebg-preview.png" alt="blueberry">
+    <h3>MIRTILO</h3>
+    <p></p>
+    <h1 class="medio">SABORES DE SORBET</h1>
 </body>
 </html>
