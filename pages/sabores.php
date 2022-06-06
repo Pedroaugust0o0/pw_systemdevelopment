ae6b759da2af02584a95079f7a2a654e6bc051be
Mariaalice04 <mariaalic345@gmail.com>
2022-06-06 11:43:17 -0300

    commit

--- pages/sabores.php
+++ pages/sabores.php
@@ -10,23 +10,18 @@
     <h1 class="menor">Nossos</h1>
     <h2 class="maior">Sabores</h2>
     <h1 class="medio">SABORES DE SORVETE</h1>
-    <img src="../img/sorveteoreo-removebg-preview.png" alt="oreo" class="oreo">
-    <h3>OREO</h3>
-    <P></P>
-    <img src="../img/chocolat-icecream-removebg-preview.png" alt="chocolate" class="chocolate">
-    <h3>CHOCOLATE</h3>
-    <p></p>
-    <img src="../img/morangosort-removebg-preview.png" alt="morango" class="morango">
-    <h3>MORANGO</h3>
-    <p></p>
-    <img src="../img/cheesecake-de-goiabada-removebg-preview.png" alt="cheesecake">
-    <h3>CHESEECAKE DE GOIABADA</h3>
-    <p></p>
-    <img src="../img/pistache-removebg-preview.png" alt="pistache">
-    <h3>PISTACHE</h3>
-    <img src="../img/blueberrysorvete-removebg-preview.png" alt="blueberry">
-    <h3>MIRTILO</h3>
-    <p></p>
+
+<div class="menu1">
+    <img src="../img/sorveteoreo-removebg-preview.png" alt="oreo" class="oreo"> <h3>OREO</h3>
+    <img src="../img/chocolat-icecream-removebg-preview.png" alt="chocolate" class="chocolate"><h3>CHOCOLATE</h3>
+    <img src="../img/morangosort-removebg-preview.png" alt="morango" class="morango"><h3>MORANGO</h3>
+</div>
+
+<div class="menu1">
+    <img src="../img/cheesecake-de-goiabada-removebg-preview.png" alt="cheesecake" class="cheesecake"> <h3>CHEESECAKE DE GOIABADA</h3>
+    <img src="../img/pistache-removebg-preview.png" alt="pistache" class="pistache"><h3>PISTACHE</h3>
+    <img src="../img/blueberrysorvete-removebg-preview.png" alt="blueberry" class="blueberry"><h3>BLUEBERRY</h3>
+</div>
     <h1 class="medio">SABORES DE SORBET</h1>
 </body>
 </html>
