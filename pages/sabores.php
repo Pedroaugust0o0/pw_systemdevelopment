c8b84cf3c8e0fffd79888c1dff4c06896cc3643a
Mariaalice04 <mariaalic345@gmail.com>
2022-06-06 13:37:08 -0300

    commit

--- pages/sabores.php
+++ pages/sabores.php
@@ -10,17 +10,17 @@
     <h1 class="menor">Nossos</h1>
     <h2 class="maior">Sabores</h2>
     <h1 class="medio">SABORES DE SORVETE</h1>
-
+<fieldset>
 <div class="menu1">
-    <img src="../img/sorveteoreo-removebg-preview.png" alt="oreo" class="oreo"> <h3>OREO</h3>
-    <img src="../img/chocolat-icecream-removebg-preview.png" alt="chocolate" class="chocolate"><h3>CHOCOLATE</h3>
-    <img src="../img/morangosort-removebg-preview.png" alt="morango" class="morango"><h3>MORANGO</h3>
+    <div><img src="../img/sorveteoreo-removebg-preview.png" alt="oreo" class="oreo"> <h3>OREO</h3></div>
+    <div><img src="../img/chocolat-icecream-removebg-preview.png" alt="chocolate" class="chocolate"> <h3>CHOCOLATE</h3></div>
+    <div><img src="../img/morangosort-removebg-preview.png" alt="morango" class="morango"> <h3>MORANGO</h3></div>
 </div>
-
+</fieldset>
 <div class="menu1">
-    <img src="../img/cheesecake-de-goiabada-removebg-preview.png" alt="cheesecake" class="cheesecake"> <h3>CHEESECAKE DE GOIABADA</h3>
-    <img src="../img/pistache-removebg-preview.png" alt="pistache" class="pistache"><h3>PISTACHE</h3>
-    <img src="../img/blueberrysorvete-removebg-preview.png" alt="blueberry" class="blueberry"><h3>BLUEBERRY</h3>
+    <div><img src="../img/cheesecake-de-goiabada-removebg-preview.png" alt="cheesecake" class="cheesecake"> <h3>CHEESECAKE DE GOIABADA</h3></div>
+    <div><img src="../img/pistache-removebg-preview.png" alt="pistache" class="pistache"> <h3>PISTACHE</h3></div>
+    <div><img src="../img/blueberrysorvete-removebg-preview.png" alt="blueberry" class="blueberry"> <h3>BLUEBERRY</h3></div>
 </div>
     <h1 class="medio">SABORES DE SORBET</h1>
 </body>
