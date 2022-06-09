8c9c4301cf9cbf177fa3734b3372b2de069f4d6b
Mariaalice04 <mariaalic345@gmail.com>
2022-06-09 11:11:08 -0300

    commit

--- pages/sabores.php
+++ pages/sabores.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../css/produto.css">
     <script src="../js/carrinho.js"></script>
+    <script src="../js/carrinho.js"></script>
     <title>Sabores</title>
 </head>
 <body>
